94222e198ae718394a5a33a028f4ccecde7358c5
zhangzeheng <743800447@qq.com>
2019-12-20 09:52:22 +0800

    添加消息controller

---
+++ src/main/java/cn/net/hlk/data/controller/NewsController.java
@@ -0,0 +1,285 @@
+package cn.net.hlk.data.controller;
+
+import cn.net.hlk.data.pojo.Page;
+import cn.net.hlk.data.pojo.PageData;
+import cn.net.hlk.data.pojo.ReasonBean;
+import cn.net.hlk.data.pojo.ResponseBodyBean;
+import cn.net.hlk.data.service.AlarmService;
+import cn.net.hlk.util.ResponseUtil;
+import cn.net.hlk.util.StringUtil2;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Title
+ * @Description 消息controller
+ * @author 张泽恒
+ * @date 2019/12/20 9:44
+ * @param
+ * @return
+ */
+@Api(description = "消息", value = "/")
+@RestController
+@RequestMapping(value="/news")
+@EnableAutoConfiguration
+public class NewsController extends BaseController{
+
+	@Autowired
+	private AlarmService alarmService;
+
+	/**
+	 * @Title: addAlarm
+	 * @discription 告警新增
+	 * @author 张泽恒       
+	 * @created 2018年10月8日 上午10:09:05     
+	 * @param pd
+	 * @param Authorization
+	 * @return
+	 */
+	@SuppressWarnings("all")
+	@ApiOperation(value = "告警新增", notes = "告警新增")
+	@ApiImplicitParams({
+		@ApiImplicitParam(paramType = "body", name = "pd", dataType = "PageData", required = true, value = "客户端传入JSON字符串", defaultValue = "") ,
+		@ApiImplicitParam(paramType = "header", name = "Authorization", dataType = "String", required = true, value = "安全中心颁发token验证信息", defaultValue = "eyJqdGkiOiIzYzdhN2UzNy0yMWUyLTRhMTYtOTUwNS0zMjVlMjI0NGMxZjAiLCJpYXQiOjE1MDM4ODc5MDcsInN1YiI6IjEiLCJpc3MiOiJTZWN1cml0eSBDZW50ZXIiLCJncm91cGNvZGUiOiIyMzAwMDAwMDAwMDAiLCJ1dHlwZSI6IjEifQ"),
+		 })
+	@ApiResponses({
+        @ApiResponse(code=200,message="指示客服端的请求已经成功收到，解析，接受"),
+        @ApiResponse(code=201,message="资源已被创建"),
+        @ApiResponse(code=401,message="未授权"),
+        @ApiResponse(code=400,message="请求参数没填好"),
+        @ApiResponse(code=403,message="拒绝访问"),
+        @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对"),
+        @ApiResponse(code=406,message="不是指定的数据类型"),
+        @ApiResponse(code=500,message="服务器内部错误")
+     })
+	@RequestMapping(value="/addAlarm", method=RequestMethod.POST)
+	public  @ResponseBody ResponseBodyBean addAlarm( @RequestBody PageData pd,  @RequestHeader String Authorization) { 
+		int status = HttpStatus.INTERNAL_SERVER_ERROR.value();//状态码
+		response.setStatus(status);//状态码存入
+		ResponseBodyBean responseBodyBean = new ResponseBodyBean();//返回值
+		ReasonBean reasonBean = new ReasonBean();//返回参数
+		PageData resData = new PageData();//返回数据
+		try{
+			if(pd != null 
+//					&& StringUtil2.isNotEmpty(pd.get("box_id"))//箱id
+					){
+				responseBodyBean = alarmService.addAlarm(pd);
+				if(responseBodyBean.getReason() == null){
+					status = HttpStatus.OK.value();
+					response.setStatus(status);
+				}
+			}else{
+				reasonBean.setCode("400");
+				reasonBean.setText("请求的参数不正确");
+				status = HttpStatus.PRECONDITION_REQUIRED.value();
+				response.setStatus(status);
+				responseBodyBean.setReason(reasonBean);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			reasonBean = ResponseUtil.getReasonBean("Exception", e.getClass().getSimpleName());
+			responseBodyBean.setReason(reasonBean);
+		}finally {
+			return responseBodyBean;
+		}
+	}
+	
+	/**
+	 * @Title: updateAlarm
+	 * @discription 告警修改
+	 * @author 张泽恒       
+	 * @created 2018年10月8日 下午1:38:40     
+	 * @param pd
+	 * @param Authorization
+	 * @return
+	 */
+	@SuppressWarnings("all")
+	@ApiOperation(value = "告警修改", notes = "告警修改")
+	@ApiImplicitParams({
+		@ApiImplicitParam(paramType = "body", name = "pd", dataType = "PageData", required = true, value = "客户端传入JSON字符串", defaultValue = "") ,
+		@ApiImplicitParam(paramType = "header", name = "Authorization", dataType = "String", required = true, value = "安全中心颁发token验证信息", defaultValue = "eyJqdGkiOiIzYzdhN2UzNy0yMWUyLTRhMTYtOTUwNS0zMjVlMjI0NGMxZjAiLCJpYXQiOjE1MDM4ODc5MDcsInN1YiI6IjEiLCJpc3MiOiJTZWN1cml0eSBDZW50ZXIiLCJncm91cGNvZGUiOiIyMzAwMDAwMDAwMDAiLCJ1dHlwZSI6IjEifQ"),
+		 })
+	@ApiResponses({
+        @ApiResponse(code=200,message="指示客服端的请求已经成功收到，解析，接受"),
+        @ApiResponse(code=201,message="资源已被创建"),
+        @ApiResponse(code=401,message="未授权"),
+        @ApiResponse(code=400,message="请求参数没填好"),
+        @ApiResponse(code=403,message="拒绝访问"),
+        @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对"),
+        @ApiResponse(code=406,message="不是指定的数据类型"),
+        @ApiResponse(code=500,message="服务器内部错误")
+     })
+	@RequestMapping(value="/updateAlarm", method=RequestMethod.POST)
+	public  @ResponseBody ResponseBodyBean updateAlarm( @RequestBody PageData pd,  @RequestHeader String Authorization) { 
+		int status = HttpStatus.INTERNAL_SERVER_ERROR.value();//状态码
+		response.setStatus(status);//状态码存入
+		ResponseBodyBean responseBodyBean = new ResponseBodyBean();//返回值
+		ReasonBean reasonBean = new ReasonBean();//返回参数
+		PageData resData = new PageData();//返回数据
+		try{
+			if(pd != null && StringUtil2.isNotEmpty(pd.get("alarm_id"))//告警id
+//					&& StringUtil2.isNotEmpty(pd.get("box_id"))//箱id
+					){
+				responseBodyBean = alarmService.updateAlarm(pd);
+				if(responseBodyBean.getReason() == null){
+					status = HttpStatus.OK.value();
+					response.setStatus(status);
+				}
+			}else{
+				reasonBean.setCode("400");
+				reasonBean.setText("请求的参数不正确");
+				status = HttpStatus.PRECONDITION_REQUIRED.value();
+				response.setStatus(status);
+				responseBodyBean.setReason(reasonBean);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			reasonBean = ResponseUtil.getReasonBean("Exception", e.getClass().getSimpleName());
+			responseBodyBean.setReason(reasonBean);
+		}finally {
+			return responseBodyBean;
+		}
+	}
+
+	/**
+	 * @Title: searchAlarm
+	 * @discription 告警条件查询
+	 * @author 张泽恒       
+	 * @created 2018年10月10日 下午3:11:10     
+	 * @param page
+	 * @param Authorization
+	 * @return
+	 */
+	@SuppressWarnings("all")
+	@ApiOperation(value = "告警条件查询", notes = "告警条件查询")
+	@ApiImplicitParams({
+		@ApiImplicitParam(paramType = "body", name = "page", dataType = "Page", required = true, value = "客户端传入JSON字符串", defaultValue = "") ,
+		@ApiImplicitParam(paramType = "header", name = "Authorization", dataType = "String", required = true, value = "安全中心颁发token验证信息", defaultValue = "eyJqdGkiOiIzYzdhN2UzNy0yMWUyLTRhMTYtOTUwNS0zMjVlMjI0NGMxZjAiLCJpYXQiOjE1MDM4ODc5MDcsInN1YiI6IjEiLCJpc3MiOiJTZWN1cml0eSBDZW50ZXIiLCJncm91cGNvZGUiOiIyMzAwMDAwMDAwMDAiLCJ1dHlwZSI6IjEifQ"),
+		 })
+	@ApiResponses({
+        @ApiResponse(code=200,message="指示客服端的请求已经成功收到，解析，接受"),
+        @ApiResponse(code=201,message="资源已被创建"),
+        @ApiResponse(code=401,message="未授权"),
+        @ApiResponse(code=400,message="请求参数没填好"),
+        @ApiResponse(code=403,message="拒绝访问"),
+        @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对"),
+        @ApiResponse(code=406,message="不是指定的数据类型"),
+        @ApiResponse(code=500,message="服务器内部错误")
+     })
+	@RequestMapping(value="/searchAlarm", method=RequestMethod.POST)
+	public  @ResponseBody ResponseBodyBean searchAlarm( @RequestBody Page page,  @RequestHeader String Authorization) { 
+		int status = HttpStatus.INTERNAL_SERVER_ERROR.value();//状态码
+		response.setStatus(status);//状态码存入
+		ResponseBodyBean responseBodyBean = new ResponseBodyBean();//返回值
+		ReasonBean reasonBean = new ReasonBean();//返回参数
+		PageData resData = new PageData();//返回数据
+		try{
+			List<PageData> pdList = new ArrayList<PageData>();
+			PageData pd = page.getPd();
+			if(page != null ){
+				if(pd != null && StringUtil2.isNotEmpty(pd.get("police_idcard"))
+//						&& StringUtil2.isNotEmpty(pd.get("menu"))//用户资源
+						){
+					responseBodyBean = alarmService.searchAlarm(page);
+					if(responseBodyBean.getReason() == null){
+						status = HttpStatus.OK.value();
+						response.setStatus(status);
+					}
+				}else{
+					reasonBean.setCode("400");
+					reasonBean.setText("请求的参数不正确");
+					status = HttpStatus.PRECONDITION_REQUIRED.value();
+					response.setStatus(status);
+					responseBodyBean.setReason(reasonBean);
+				}
+			}else{
+				reasonBean.setCode("400");
+				reasonBean.setText("请求的参数不正确");
+				status = HttpStatus.PRECONDITION_REQUIRED.value();
+				response.setStatus(status);
+				responseBodyBean.setReason(reasonBean);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			reasonBean = ResponseUtil.getReasonBean("Exception", e.getClass().getSimpleName());
+			responseBodyBean.setReason(reasonBean);
+		}finally {
+			return responseBodyBean;
+		}
+	}
+
+	/**
+	 * @Title: getAlarmInfomationById
+	 * @discription 根据告警id 获取告警信息
+	 * @author 张泽恒       
+	 * @created 2018年10月19日 上午8:54:49     
+	 * @param pd
+	 * @param Authorization
+	 * @return
+	 */
+	@SuppressWarnings("all")
+	@ApiOperation(value = "根据告警ID获取信息", notes = "根据告警ID获取告警信息")
+	@ApiImplicitParams({
+		@ApiImplicitParam(paramType = "body", name = "pd", dataType = "PageData", required = true, value = "客户端传入JSON字符串", defaultValue = "") ,
+		@ApiImplicitParam(paramType = "header", name = "Authorization", dataType = "String", required = true, value = "安全中心颁发token验证信息", defaultValue = "eyJqdGkiOiIzYzdhN2UzNy0yMWUyLTRhMTYtOTUwNS0zMjVlMjI0NGMxZjAiLCJpYXQiOjE1MDM4ODc5MDcsInN1YiI6IjEiLCJpc3MiOiJTZWN1cml0eSBDZW50ZXIiLCJncm91cGNvZGUiOiIyMzAwMDAwMDAwMDAiLCJ1dHlwZSI6IjEifQ"),
+		 })
+	@ApiResponses({
+        @ApiResponse(code=200,message="指示客服端的请求已经成功收到，解析，接受"),
+        @ApiResponse(code=201,message="资源已被创建"),
+        @ApiResponse(code=401,message="未授权"),
+        @ApiResponse(code=400,message="请求参数没填好"),
+        @ApiResponse(code=403,message="拒绝访问"),
+        @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对"),
+        @ApiResponse(code=406,message="不是指定的数据类型"),
+        @ApiResponse(code=500,message="服务器内部错误")
+     })
+	@RequestMapping(value="/getAlarmInfomationById", method=RequestMethod.POST)
+	public  @ResponseBody ResponseBodyBean getAlarmInfomationById( @RequestBody PageData pd,  @RequestHeader String Authorization) { 
+		int status = HttpStatus.INTERNAL_SERVER_ERROR.value();//状态码
+		response.setStatus(status);//状态码存入
+		ResponseBodyBean responseBodyBean = new ResponseBodyBean();//返回值
+		ReasonBean reasonBean = new ReasonBean();//返回参数
+		PageData resData = new PageData();//返回数据
+		try{
+			if(pd != null 
+//					&& StringUtil2.isNotEmpty(pd.get("box_id"))//箱id
+					){
+				responseBodyBean = alarmService.getAlarmInfomationById(pd);
+				if(responseBodyBean.getReason() == null){
+					status = HttpStatus.OK.value();
+					response.setStatus(status);
+				}
+			}else{
+				reasonBean.setCode("400");
+				reasonBean.setText("请求的参数不正确");
+				status = HttpStatus.PRECONDITION_REQUIRED.value();
+				response.setStatus(status);
+				responseBodyBean.setReason(reasonBean);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			reasonBean = ResponseUtil.getReasonBean("Exception", e.getClass().getSimpleName());
+			responseBodyBean.setReason(reasonBean);
+		}finally {
+			return responseBodyBean;
+		}
+	}
+	
+
+}
